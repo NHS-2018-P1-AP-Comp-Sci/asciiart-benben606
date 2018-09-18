770b4647769c79b7fb8d890df3d05c0788180fa8
s-liuben <s-liuben@53S50029671.it.bsd405.org>
2018-09-18 15:19:18 -0700

    Made methods to make the three little pigs and added grass beneath their feet!

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,17 +1,57 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
+public class ASCIIArt {
 
-// YOUR CODE GOES HERE :)
+	public static void printHead () {
+		System.out.println("     --.        --.");
+		System.out.println("   _'   \"      \" ' _");
+		System.out.println("   \\  >   -   -' <  /");
+		System.out.println("     '-.          .-'");
+		System.out.println("       / 'e___e' \\");
+		System.out.println("      (   (o o)   )");
+		System.out.println("       \\_  '='  _/");
+		printBody();
+		printFeet();
+	}
+	
+	public static void printBody () {
+		System.out.println("     / /|`-._.-'|\\ \\");
+		System.out.println("    / /||_______||\\ \\");
+		System.out.println("   / /_||=======||_\\ \\");
+		System.out.println(" / _/==||       ||==\\_ \\");
+		System.out.println("   (   ^^       ^^   )");
+		System.out.println("    \\               /");
+		System.out.println("     \\______|______/");
+	}
+	
+	public static void printFeet () {
+		System.out.println("     |______|______|");
+		System.out.println("       )__|   |__(");
+		System.out.println("      /   ]   [   \\");
+		System.out.println("      '--'     '--'");
+	}
+	
+	public static void Space () {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void Grass () {
+		System.out.println("/////////////////////////");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printHead();
+		Grass();
+		Space();
+		printHead();
+		Grass();
+		Space();
+		printHead();
+		Grass();
+		
+	}
+	
+	
+}
+	
